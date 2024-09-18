ad21d00e4af15ce049b8a6c50955ca2736745aad
DavidIraheta <david.iraheta.89@gmail.com>
2024-09-18 10:45:39 -0600

    Add files via upload

---
+++ 07_06_whats_my_type.py
@@ -0,0 +1,11 @@
+# What data type is the variable `mystery` at the end of the script?
+# What data types does it hold at certain points earlier in the script?
+
+mystery = None
+type(None) = Nonetype
+mystery = "Sommerfeld"
+type("Sommerfield") = Script
+mystery = 137
+type(137) = Int
+mystery = mystery + 0.0 
+type(0.0) = float
